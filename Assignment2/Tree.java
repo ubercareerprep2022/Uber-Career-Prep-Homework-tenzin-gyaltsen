138022fcf00e5c2b048e2036d8ba76f838e36318
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-05-24 22:02:53 -0500

    Finished Exercise 1

--- Assignment2/Tree.java
+++ Assignment2/Tree.java
@@ -7,17 +7,26 @@ public class Tree {
       this.root = root;
     }
 
-    public void printTree() {
+    public void inOrderTraversal (TreeNode node) {
+      if (node == null) {
+        return;
+      }
 
+      inOrderTraversal((node.left));
+      System.out.println(node.data + " ");
+      inOrderTraversal(node.right);
     }
 
     public static void main(String[] args) {
-        TreeNode leftChild = new TreeNode(6, null, null);
-        TreeNode rightChild = new TreeNode(3, null, null);
-        TreeNode left = new TreeNode(7, null, null);
-        TreeNode right = new TreeNode(17, leftChild, rightChild);
-        TreeNode root = new TreeNode(1, left, right);
-        Tree tree = new Tree(root);
+      TreeNode leftChild = new TreeNode(6, null, null);
+      TreeNode rightChild = new TreeNode(3, null, null);
+      TreeNode left = new TreeNode(7, null, null);
+      TreeNode right = new TreeNode(17, leftChild, rightChild);
+      TreeNode root = new TreeNode(1, left, right);
+      Tree tree = new Tree(root);
+
+      System.out.println("Tree Nodes in Inorder Traversal");
+      tree.inOrderTraversal(tree.root);
     }
 }
   
