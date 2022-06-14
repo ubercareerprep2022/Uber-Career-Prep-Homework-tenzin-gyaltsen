79a4590e7fd8b31594b7742f3ef0d239c97bca22
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-06-13 22:09:28 -0500

    Finished print algorithm for organizational structure by level

--- Assignment2/OrganizationStructure.java
+++ Assignment2/OrganizationStructure.java
@@ -1,6 +1,6 @@
 package Assignment2;
+
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.*;;
@@ -8,8 +8,8 @@ import java.util.*;;
 public class OrganizationStructure {
     public Employee ceo;
     private Queue<Employee> employees = new LinkedList<>();
-     
-    public OrganizationStructure (Employee ceo) {
+
+    public OrganizationStructure(Employee ceo) {
         this.ceo = ceo;
         employees.add(ceo);
     }
@@ -19,57 +19,74 @@ public class OrganizationStructure {
         public String title;
         private List<Employee> directReports = new ArrayList<>();
 
-        public void addDirectReports (Employee dEmployee) {
+        public void addDirectReports(Employee dEmployee) {
             directReports.add(dEmployee);
         }
     }
 
-    // if the employee has a list of direct reporter, then print the list of employees
-    // then, go by each direct reportee, and check if it also has direct reportee, and follow the same pattern
+    // if the employee has a list of direct reporter, then print the list of
+    // employees
+    // then, go by each direct reportee, and check if it also has direct reportee,
+    // and follow the same pattern
     // if they don't, we don't print
-
-    public void printLevelByLevel () {
+    public void printLevelByLevel() {
+        int employeesAtSameLevel = 1;
         while (!employees.isEmpty()) {
             // pop the queue and print the employee
-            // if that employee has direct reportee, 
-                // check if each employee has direct reportee
-                // add direct report to the queue
+            // if that employee has direct reportee,
+            // check if each employee has direct reportee
+            // add direct report to the queue
             Employee cEmployee = employees.poll();
-            System.out.print(cEmployee.title + " "+ cEmployee.name);
+            employeesAtSameLevel--;
+            System.out.print(cEmployee.title + "   ");
+
+            if (employeesAtSameLevel == 0 && !(employeesAtSameLevel < 0)) {
+                System.out.println();
+                employeesAtSameLevel += employees.size() + cEmployee.directReports.size();
+            } 
+
             if (cEmployee.directReports.size() > 0) {
                 for (Employee e : cEmployee.directReports) {
                     employees.add(e);
                 }
             }
-        } 
+        }
     }
-
 
     public static void main(String[] args) {
 
         Employee ceo = new Employee();
-        ceo.name = "A";
+        ceo.name = "Braden";
         ceo.title = "CEO";
 
         OrganizationStructure sparrow = new OrganizationStructure(ceo);
 
         // subemployee
         Employee cfo = new Employee();
-        cfo.name = "B";
+        cfo.name = "Gautam";
         cfo.title = "CFO";
 
         Employee cto = new Employee();
-        cto.name = "C";
+        cto.name = "Shubham";
         cto.title = "CTO";
 
         ceo.addDirectReports(cfo);
         ceo.addDirectReports(cto);
 
-        Employee managerOne = new Employee();
-        managerOne.name = "D";
-        managerOne.title = "M";
+        Employee finAdvisor = new Employee();
+        finAdvisor.name = "Raju";
+        finAdvisor.title = "Advisor";
+        cfo.addDirectReports(finAdvisor);
 
-        cto.addDirectReports(managerOne);
+        Employee manager = new Employee();
+        manager.name = "Rahul";
+        manager.title = "Manager";
+        cto.addDirectReports(manager);
+
+        Employee assistant = new Employee();
+        assistant.name = "Tashi";
+        assistant.title = "Assistant";
+        manager.addDirectReports(assistant);
 
         sparrow.printLevelByLevel();
     }
