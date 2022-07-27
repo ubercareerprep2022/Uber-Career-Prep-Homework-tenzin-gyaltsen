7f5b4b03bc6adadd52b77333f8f14f214d9c8c06
gyaltsentenzin <40735388+gyaltsentenzin@users.noreply.github.com>
2022-07-26 19:21:58 -0500

    Working on DFS

--- Assignment2/GraphWithAdjacencyList.java
+++ Assignment2/GraphWithAdjacencyList.java
@@ -3,6 +3,7 @@ package Assignment2;
 import java.util.*;
 
 
+
 public class GraphWithAdjacencyList {
 
     public static class GraphNode {
@@ -18,6 +19,7 @@ public class GraphWithAdjacencyList {
 
     public GraphWithAdjacencyList(){
         adjNodes = new HashMap<>();
+        GraphNodeIdentifier = new HashMap<>();
     }
 
     public void addNode(int key){
@@ -31,20 +33,52 @@ public class GraphWithAdjacencyList {
 
     public void addEdge(int node1, int node2){
         if(GraphNodeIdentifier.containsKey(node1) && GraphNodeIdentifier.containsKey(node2)){
-            adjNodes.get(new GraphNode(node1)).add(new GraphNode(node2));
-            adjNodes.get(new GraphNode(node2)).add(new GraphNode(node1));
+            for(GraphNode node : adjNodes.keySet()){
+                if(node.data == node1){
+                    adjNodes.get(node).add(new GraphNode(node2));
+                }
+                if(node.data == node2){
+                    adjNodes.get(node).add(new GraphNode(node1));
+                }
+            }
         }
     }
 
     public void removeEdge(int node1, int node2){
-        if(adjNodes.containsKey(new GraphNode(node1)) && adjNodes.containsKey(new GraphNode(node2))){
-            adjNodes.get(new GraphNode(node1)).remove(new GraphNode(node2));
-            adjNodes.get(new GraphNode(node2)).remove(new GraphNode(node1));
+        if(GraphNodeIdentifier.containsKey(node1) && GraphNodeIdentifier.containsKey(node2)){
+            Set<GraphNode> kSet = adjNodes.keySet();
+            for(GraphNode node : kSet){
+                if(node.data == node1){
+                    List<GraphNode> edges = adjNodes.get(node);
+                    GraphNode rNode = new GraphNode(-1);
+                    for(GraphNode edge : edges){
+                        if(edge.data == node2){
+                            rNode = edge;
+                        }
+                    }
+                    adjNodes.get(node).remove(rNode);
+                }
+                if(node.data == node2){
+                    List<GraphNode> edges = adjNodes.get(node);
+                    GraphNode rNode = new GraphNode(-1);
+                    for(GraphNode edge : edges){
+                        if(edge.data == node1){
+                            rNode = edge;
+                        }
+                    }
+                    adjNodes.get(node).remove(rNode);
+                }
+            }
         }
     }
 
     public List<GraphNode> getAdjNodes(int key){
-        return adjNodes.get(new GraphNode(key));
+        for(GraphNode n : adjNodes.keySet()){
+            if(n.data == key){
+                return adjNodes.get(n);
+            }
+        }
+        return null;
     }
 
     public void printAdjList(){
@@ -59,16 +93,49 @@ public class GraphWithAdjacencyList {
         }
     }
 
+    public void DFS(int key){
+        Stack<Integer> aGraphNodes = new Stack<>();
+        for(GraphNode n : adjNodes.keySet()){
+            if(n.data == key){
+                System.out.print(n.data + " ");
+                for(GraphNode edge : adjNodes.get(n)){
+                    aGraphNodes.add(edge.data);
+                }
+            }
+        }
+
+        Iterator<Integer> iT = aGraphNodes.iterator();
+        while(iT.hasNext()){
+            Integer curData = aGraphNodes.pop();
+            System.out.print(curData + " ");
+            for(GraphNode n : returnEdgeList(curData)){
+                if(!aGraphNodes.contains(n.data)){
+                    aGraphNodes.add(n.data);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public List<GraphNode> returnEdgeList(int key){
+        for(GraphNode n : adjNodes.keySet()){
+            if(n.data == key){
+                return adjNodes.get(n);
+            }
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Start");
         GraphWithAdjacencyList graph = new GraphWithAdjacencyList();
         graph.addNode(1);
         graph.addNode(2);
+        graph.addNode(3);
         graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
         graph.printAdjList();
-        System.out.println("end");
-        System.out.println(graph.getAdjNodes(1));
+        graph.DFS(1);
     }
 }
 
